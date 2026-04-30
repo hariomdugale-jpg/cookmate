13f15327b50e5b77900c5bec931c6e81c6a62196
hariom ugale <hariomdugale@gmail.com>
2026-04-30 13:18:46 +0530

    cookmate

---
+++ cdc.c
@@ -0,0 +1,18 @@
+/*#include <stdio.h>
+
+int main() {
+    int num = 5;
+    {
+        int num = 10;
+        printf("%d ", num);
+    }
+    printf("%d", num);
+    return 0;
+}*/
+#include <stdio.h>
+
+int main() {
+    float num = 10 / 3;
+    printf("%f\n", num);
+    return 0;
+}
